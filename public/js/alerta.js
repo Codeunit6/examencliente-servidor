c55cfc76faa2452e98ad08092468e0059aa4c559
Codeunit6 <fernando.brayan.m.g@gmail.com>
2022-01-31 05:10:36 -0600

    Actualizacion

--- public/js/alerta.js
+++ public/js/alerta.js
@@ -1,10 +1,20 @@
 $(document).ready(function() {
     $("#unidad2").click(function(){
-        alert('No se puede acceder aqui por que aun no esta configurado');
+        Swal.fire({
+            icon: 'error',
+            title: 'Los datos no han sido encontrados',
+            text: 'Esta seccion no ha sido configurada, entra en otra seccion',
+            footer: 'ENTRA A LA SECCION UNIDAD 1'
+          });
     }); 
 });
 $(document).ready(function() {
     $("#unidad3").click(function(){
-        alert('No se puede acceder aqui por que aun no esta configurado');
+        Swal.fire({
+            icon: 'error',
+            title: 'Los datos no han sido encontrados',
+            text: 'Esta seccion no ha sido configurada, entra en otra seccion',
+            footer: 'ENTRA A LA SECCION UNIDAD 1'
+          });
     }); 
 });
